8f8c10d323cf04e1bfd3c41c2799349cb00c2b28
sauravk7077 <sauravk7077@gmail.com>
2020-07-01 12:29:45 +0530

    changed the ui and added icons

--- typescripts/frame.ts
+++ typescripts/frame.ts
@@ -1,18 +1,32 @@
 $(document).ready(() => {
     let url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-    let api_key = 'Your_API_Key';
+    let imgUrl = 'https://openweathermap.org/img/wn/';
+    let api_key = '37540f937e26e3fe35860c835ecb224a';
     let units = "metric";
     function addLoader() {
         $('.container').append(`<div class="preloader">
-        <img src="static/loader.png"/>
+        <img src="static/preloader.png"/>
     </div> `);
     };
     function removeLoader() {
         $('.preloader').remove();
     };
+    function hideColumnBox() {
+        if (!$('#hidden').hasClass('hide'))
+            $('#hidden').addClass('hide');
+        if($('#hidden').hasClass('columnBox'))
+            $('#hidden').removeClass('columnBox');
+    }
+    function showColumnBox() {
+        if ($('#hidden').hasClass('hide'))
+            $('#hidden').removeClass('hide');
+        if(!$('#hidden').hasClass('columnBox'))
+            $('#hidden').addClass('columnBox');
+    }
     function handleSearch(e:KeyboardEvent) {
         if(e.keyCode == 13) {
             $('.container .error404').remove();
+            hideColumnBox();
             addLoader();
             let city = $('.search').val();
             $.get(`${url}${city}&appid=${api_key}&units=${units}`)
@@ -27,17 +41,49 @@ $(document).ready(() => {
             })
         }
     };
-
     function handleData(data) {
+        $.get('/static/icons.json').then((jsonData) =>{
+            modifiyTemplate(data, jsonData);
+        });
+    };
+
+    function modifiyTemplate(data, icons) {
         $('.address').text(`${data['name']}, ${data['sys']['country']}`);
+        //Handling Date
         let date = new Date();
         let day = date.toLocaleString('en-us', {  weekday: 'long' });
         $('.day').text(day);
         $('.date').text(`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`);
-        $('#mainImg').attr('src', '');
+
+        $('#mainImg').removeClass();
+        $('#mainImg').addClass(`${getIcon(data, icons)}`);
+        
         $('.tempNow').text(`${data['main']["temp"]} ℃`);
-        $('.ppt').text(`${data['main']['humidity']} mm`)
-    };
+        $('.desc').text(data.weather[0].description);
+        $('.humidity').text(`${data['main']['humidity']} %`)
+        $('.maxTemp').text(`${data['main']['temp_max']} ℃`);
+        $('.minTemp').text(`${data['main']['temp_min']} ℃`);
+        $('.pressure').text(`${data['main']['pressure']} hPa`);
+        $('.windSpeed').text(`${data.wind.speed} ms⁻¹`);
+        $('.winddirection').text(`${data.wind.deg}°`);
+        $('.windDirImg').removeClass();
+        $('#windDirImg').addClass(`wi wi-wind towards-${data.wind.deg}-deg`);
+        showColumnBox();
+    }
+    function getIcon(resp, weatherIcons){
+        var prefix = 'wi wi-';
+        var code = resp.weather[0].id;
+        var icon = weatherIcons[code].icon;
+
+        // If we are not in the ranges mentioned above, add a day/night prefix.
+        if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+            icon = 'day-' + icon;
+        }
+
+        // Finally tack on the prefix.
+        icon = prefix + icon;
+        return icon;
+    }
 
     $('.search').keydown(handleSearch);
 });
